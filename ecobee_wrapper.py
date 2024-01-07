062faf3293aeaec531a45330e06ee509ef396d4e
jastec <jacob.stec@nokia.com>
2024-01-06 19:59:16 -0500

    update to use api wrapper

---
+++ ecobee_wrapper.py
@@ -0,0 +1,47 @@
+import requests
+import urllib.parse
+import logging
+import os
+
+class EccobeeWrapper:
+    
+    def __init__(self, hostname: str, ver: str = '1'):
+
+        self.url = "https://{}/{}/".format(hostname, ver)
+        self._fetchAccesToken()
+
+    def _fetchAccesToken(self):
+
+        data = {
+        'grant_type': 'refresh_token',
+        'code': os.environ['REFRESH_TOKEN'],
+        'client_id': os.environ['API_KEY'],
+        }
+
+        try:
+            response = requests.post(url="https://api.ecobee.com/token", data = data)
+        except requests.exceptions.RequestException as e:
+            logging.error(e)
+
+        self.access_token = response.json().get('access_token')
+    
+    def _doRequest(self, http_method: str, endpoint: str, ep_params: dict = None, data: str = None):
+        
+        full_url = self.url + endpoint
+        headers = {'Content-Type': 'application/json;charset=UTF-8', 'Authorization': 'Bearer ' + self.access_token}
+        # ecobee API asks that JSON data be URL encoded for optimal comptability
+        if 'body' in ep_params:
+            ep_params['body'] = urllib.parse.quote_plus(ep_params['body'])
+        if data:
+            data = urllib.parse.quote_plus(data)
+        response = requests.request(method=http_method, url=full_url, headers=headers, params=ep_params, data=data)
+
+        return response.json()
+
+    def get(self, endpoint: str, ep_params: dict = None) -> list[dict]:
+
+        return self._doRequest(http_method='GET', endpoint=endpoint, ep_params=ep_params)
+    
+    def post(self, endpoint: str, ep_params: dict = None, data: str = None):
+
+        return self._doRequest(http_method='POST', endpoint=endpoint, ep_params=ep_params, data=data)
